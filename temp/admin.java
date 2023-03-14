4fd19f4825f5d5cd10ce3424e1b22c48b68b2e7e
Micael <98262812+MicaelSv@users.noreply.github.com>
2023-03-14 13:59:56 -0300

    Add files via upload

---
+++ temp/admin.java
@@ -0,0 +1,25 @@
+public class admin extends conta {
+
+    public admin(String email, String senha, String nome, int id) {
+        super(email, senha, nome, id);
+    }
+
+    public String plano;
+    public int limitelivro = 0;
+
+    @Override
+    public void defPlano(String planoesc) {
+        this.plano = planoesc;
+    }
+
+    @Override
+    public String getPlano() {
+        return this.plano;
+    }
+
+    @Override
+    public int getLimlivro() {
+        return this.limitelivro;
+    }
+
+}
---
+++ temp/premium.java
@@ -0,0 +1,46 @@
+public class premium extends conta {
+
+    public premium(String email, String senha, String nome, int id) {
+        super(email, senha, nome, id);
+    }
+
+    public String plano, bios;
+    public String valormulta = "2 reais";
+    public String tempomulta = "30 dias";
+    public int limitelivro = 15, biostam = 200;
+
+    @Override
+    public void defPlano(String planoesc) {
+        this.plano = planoesc;
+    }
+
+    @Override
+    public String getPlano() {
+        return this.plano;
+    }
+
+    @Override
+    public int getLimlivro() {
+        return this.limitelivro;
+    }
+
+    @Override
+    public String getValormulta() {
+        return this.valormulta;
+    }
+
+    @Override
+    public String getTempomulta() {
+        return this.valormulta;
+    }
+
+    @Override
+    public void defBios(String bio) {
+        this.bios = bio;
+    }
+
+    @Override
+    public int getBiotam() {
+        return this.biostam;
+    }
+}
